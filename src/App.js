19163893aace086bfdf5d821d60ae016b5b0eacc
Tetsu-is <hap30305@kwansei.ac.jp>
2023-05-04 00:10:03 +0900

    react7 done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@
 
 import * as React from 'react'
 import './App.css'
+import { useState } from 'react'
 
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/pointer-german/n02100236_2542.jpg");
+  const handleButtonClick = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(dogUrl => setDogUrl(dogUrl.message))
+      .catch(error => console.log(error))
+  };
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header className='header'>Dogアプリ</header>
+      <p className='p'>This is for people who love dogs.</p>
+      <img src={dogUrl} />
+      <br></br>
+      <button onClick={handleButtonClick}>更新</button>
     </div>
   )
 }
