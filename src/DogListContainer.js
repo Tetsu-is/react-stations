97b678a5747fe3ce16cb4a73e0ab528a0123918a
Tetsu-is <hap30305@kwansei.ac.jp>
2023-05-07 16:19:39 +0900

    a

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,9 +1,35 @@
 // DO NOT DELETE
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const DogListContainer = () => {
-    const [breed, setBreed] = useState();
+
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    const fetchDogBreeds = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/list/all");
+        const result = await response.json();
+
+        // APIレスポンスから犬種リストを生成
+        const breedsArray = Object.keys(result.message);
+        setBreeds(breedsArray);
+      } catch (error) {
+        console.error("Error fetching dog breeds:", error);
+      }
+    };
+
+    fetchDogBreeds();
+  }, []); // 空の配列を渡すことで、このuseEffectはコンポーネントがマウントされたときに一度だけ実行されます
+
   return (
-    <div>DogListContainer</div>
-  )
-}
+    <div>
+      <h1>Dog Breeds List:</h1>
+      <ul>
+        {breeds.map((breed, index) => (
+          <li key={index}>{breed}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
